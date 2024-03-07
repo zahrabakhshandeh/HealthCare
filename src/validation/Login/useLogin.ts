325102ad8d56ba515894a6c20e29179736bfd383
MohammadMosavat <42.octmst@gmail.com>
2024-03-07 17:09:11 +0330

    Add validation folder for vlidation of login form and sign up form

---
+++ src/validation/Login/useLogin.ts
@@ -0,0 +1,43 @@
+import * as yup from "yup";
+// import { toast } from 'react-toastify';
+
+import { LoginDataType } from "@/types";
+import { useCallback } from "react";
+import { toast } from "react-toastify";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useRouter } from "next/router";
+
+export const loginSchema = yup.object({
+  phoneNumber: yup
+    .string()
+    .length(11, "شماره تماس باید ۱۱ عدد داشته باشد")
+    .matches(/(0|\+98)?([ ]|-|[()]){0,2}9[1|2|3|4]([ ]|-|[()]){0,2}(?:[0-9]([ ]|-|[()]){0,2}){8}/ig , 'شماره تماس را به درستی وارد کنید')
+    .required("شماره تماس الزامی است"),
+});
+
+const useLogin = () => {
+  const {
+    control,
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<LoginDataType>({
+    resolver: yupResolver(loginSchema),
+  });
+
+
+  const handelValueInputs = useCallback((data: LoginDataType) => {
+    console.log(data);
+  }, []);
+
+  return {
+    control,
+    handelValueInputs,
+    register,
+    errors,
+    handleSubmit,
+  };
+};
+
+export default useLogin;
