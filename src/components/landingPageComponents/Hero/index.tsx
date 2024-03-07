9843fb42931529fb20929395103d8e6c0fae5757
ParsaBordbar <parsab71@gmail.com>
2024-03-07 17:51:50 +0330

    Add Responsiveness to xl and 2xl For NavarBar & Here components. Change .gitignore file.

--- src/components/landingPageComponents/Hero/index.tsx
+++ src/components/landingPageComponents/Hero/index.tsx
@@ -1,18 +1,21 @@
 import MainButton from "@/components/MainButton";
 import HeroSvg from "/public/svg/hero.svg"
+
 const Hero = () => {
     return(
-        <div className="flex justify-center">
-            <div className="grid grid-cols-2 px-10 pt-40 bg-[var(--primary-blue)] w-full">
-                <div className="flex flex-col xl:gap-32 items-start">
-                    <h2 className="xl:text-8xl lg:text-2xl text-white lg:px-40 md:px-16">
+        <div className="flex justify-center items-center">
+            <div className="grid grid-cols-2 xl:px-10 xl:pt-40 bg-[var(--primary-blue)] w-full">
+
+                <div className="flex flex-col xl:gap-32 items-start col-span-1">
+                    <h2 className="xl:text-8xl lg:text-2xl text-white 2xl:px-36 md:px-16">
                         سلامتی «شما» 
                         برای «ما»
                         مهم است.
                     </h2>
-                    <MainButton value="شروع کنیم!" className="bg-[var(--green-btn)] w-1/3 h-20 mx-36"/>
+                    <MainButton value="شروع کنیم!" className="bg-[var(--green-btn)] w-1/3 h-20 lg:mx-36"/>
                 </div>
-                <div className="lg:scale-75 xl:scale-100">
+                
+                <div className="lg:scale-75 xl:scale-100 flex xl:gap-32 items-center col-span-1">
                     <HeroSvg />
                 </div>
             </div>
