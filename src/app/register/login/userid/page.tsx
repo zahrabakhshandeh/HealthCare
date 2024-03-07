49d74c28118cc802b5568b0ff54fcdc036dc42fe
MohammadMosavat <42.octmst@gmail.com>
2024-03-07 18:05:13 +0330

    Add folder to show us identify user page

---
+++ src/app/register/login/userid/page.tsx
@@ -0,0 +1,12 @@
+import FormIdentifyUser from "@/components/FromIndentifyUser";
+
+const IdentifyUser = () => {
+  return (
+    <>
+      <h2 className="text-2xl font-semibold">کد را وارد کنید</h2>
+      <FormIdentifyUser />
+    </>
+  );
+};
+
+export default IdentifyUser;
