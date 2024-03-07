f12f0291d6e3609d8d486eeebb6127be8678c1ca
ParsaBordbar <parsab71@gmail.com>
2024-03-07 09:19:43 +0330

    Initial commit. Add project hierarchy. Add components dir. Add types dir. Add signup-login page dir. NOTE: this will be updated in branches.

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import LoginArt from '/public/login.svg'
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        Health Care
+      </div>
+    </main>
+  );
+}
---
+++ src/components/MainButton/index.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { TButton } from "@/types";
+import { useCallback } from "react";
+
+const MainButton = (props: TButton) => {
+  const checkIcon = useCallback(() => {
+    if (props.iconSrc) return <props.iconSrc />;
+  }, []);
+
+  return (
+    <button
+      className={`${props.className} flex  ${
+        props.iconSrc && "gap-2"
+      } items-center justify-center  drop-shadow-md  p-2 rounded-lg h-12`}
+    >
+      {checkIcon()}
+      <span className="!text-white font-semibold  text-2xl  ">
+        {props.value}
+      </span>
+    </button>
+  );
+};
+
+export default MainButton;
