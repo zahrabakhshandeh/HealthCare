5795b5e558e5bd6ae24ac68f4f4a27140182f083
MohammadMosavat <42.octmst@gmail.com>
2024-03-08 11:43:27 +0330

    move page of signup into signup folder

--- src/components/BgRegister/index.tsx
+++ src/components/BgRegister/index.tsx
@@ -3,7 +3,7 @@ import { memo } from "react";
 const BackgroundRegister = (props: { loginMode: boolean }) => {
   return (
     <div
-      className={`z-10 w-[75vw] h-screen bg-[#4648bc] ${
+      className={` w-[75vw] h-screen bg-[#4648bc] ${
         props.loginMode
           ? "lg:flex hidden left-0 rounded-e-none rounded-s-[3.1rem]"
           : "absolute right-0 rounded-e-[3.1rem] rounded-s-none lg:flex hidden"
