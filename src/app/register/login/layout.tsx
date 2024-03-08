059539dc2527f48c573f98a99c618dfd252aa21f
MohammadMosavat <42.octmst@gmail.com>
2024-03-08 11:54:57 +0330

    now the box of login and identify user is center by flex

--- src/app/register/login/layout.tsx
+++ src/app/register/login/layout.tsx
@@ -6,15 +6,15 @@ import { ReactNode } from "react";
 const LayoutLoginPage = ({ children }: { children: ReactNode }) => {
   return (
     <section className="flex relative items-center h-screen justify-center item lg:justify-start lg:bg-[var(--hr-gray)]">
-      <main className="w-full">
+      <main className="w-full flex items-center">
         <div
-          className={`rounded-[5rem] bg-white left-0 z-20 relative lg:absolute lg:w-[667px]  py-10 px-12 w-full lg:left-auto lg:bottom-auto lg:right-[5%] lg:top-[25%] gap-10 flex flex-col bottom-0 right-0 top-0 scale-75 `}
+          className={`rounded-[5rem] bg-white left-0 z-20 relative lg:absolute lg:w-[667px]  py-10 px-12 w-full lg:left-auto  lg:right-[5%]  gap-10 flex flex-col  right-0 scale-75 `}
         >
           {children}
         </div>
       </main>
-      <div className="hidden lg:inline-block">
-        <div className="absolute flex flex-col gap-4 z-10 top-[15%] left-[8%]">
+      <div className="hidden lg:flex lg:items-center">
+        <div className="absolute flex flex-col gap-4 z-10 left-[8%]">
           <LoginArt />
           <div className=" relative bottom-0 flex flex-col items-center">
             <span className="text-white font-semibold w-3/5 text-center text-base">
