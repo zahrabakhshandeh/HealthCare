a84e2876330ae1fda6e77dbe2324ee6f27adcf63
MohammadMosavat <42.octmst@gmail.com>
2024-03-08 11:48:27 +0330

    now all elemnts include signupbox and signupart is center

--- src/app/register/signup/page.tsx
+++ src/app/register/signup/page.tsx
@@ -5,10 +5,10 @@ import BackgroundRegister from "@/components/BgRegister";
 const SignUpPage = () => {
   return (
     <section className="flex relative h-screen justify-center item lg:justify-start lg:bg-[var(--hr-gray)]">
-      <div className="hidden lg:inline-block">
-        <div className="absolute flex flex-col gap-4 z-10 top-[10%] right-[10%]">
+      <div className="hidden lg:flex lg:items-center">
+        <div className="absolute flex justify-center flex-col gap-4 z-10 right-[10%]">
           <SignUpArt />
-          <div className=" relative bottom-0 flex flex-col items-center">
+          <div className=" relative bottom-0 flex flex-col">
             <span className="text-[var(--hr-gray)] font-semibold text-base">
               سلامتی؛ انتخابی برای زندگی، نه تنها یک هدف.
             </span>
