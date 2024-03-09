2de03f7b3d18379ac50682c2ed7c070f7137da4a
MohammadMosavat <42.octmst@gmail.com>
2024-03-09 22:23:08 +0330

    change the size of text of body in texbox

--- src/components/landingPageComponents/TextBox/index.tsx
+++ src/components/landingPageComponents/TextBox/index.tsx
@@ -4,7 +4,7 @@ const TextBox = ({ tittle, body }: TextBoxType) => {
   return (
     <div className="flex gap-12 items-center md:px-36 px-14 flex-col min-w-6">
       <h2 className="text-7xl lg:text-6xl">{tittle}</h2>
-      <p className="xl:text-2xl lg:text-lg text-center text-3xl align-middle">
+      <p className="xl:text-2xl lg:text-lg text-center text-2xl align-middle">
         {body}
       </p>
     </div>
