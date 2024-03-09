a525b2f632c5fef20c9de8c9ddf0b1fa2fd60ffd
MohammadMosavat <42.octmst@gmail.com>
2024-03-09 23:45:09 +0330

    Add type for joinUsDataType that equal to LoginDataType

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,77 +1,81 @@
-import {ButtonHTMLAttributes, FunctionComponent, InputHTMLAttributes} from "react";
+import {
+  ButtonHTMLAttributes,
+  FunctionComponent,
+  InputHTMLAttributes,
+} from "react";
 
 export interface QuickLinkBoxTypes {
-    subject:String, 
-    bodyText:String, 
-    badgeLogo?:FunctionComponent|any,
-    badgeColor?: String,
+  subject: String;
+  bodyText: String;
+  badgeLogo?: FunctionComponent | any;
+  badgeColor?: String;
 }
 
-export interface MessageType{
-    type: String,
-    text: String
+export interface MessageType {
+  type: String;
+  text: String;
 }
 
-export interface ProfileType{
-    avatar?: string|any,
-    userName: string
+export interface ProfileType {
+  avatar?: string | any;
+  userName: string;
 }
 
-export interface sideBarElementType{
-    text: string, 
-    logo?:FunctionComponent|any,
-    link?: string 
+export interface sideBarElementType {
+  text: string;
+  logo?: FunctionComponent | any;
+  link?: string;
 }
-  
+
 export type TButton = ButtonHTMLAttributes<HTMLButtonElement> & {
-value: string;
-className?: string;
-iconSrc?: FunctionComponent;
+  value: string;
+  className?: string;
+  iconSrc?: FunctionComponent;
 };
 
 export type TInput = InputHTMLAttributes<HTMLDivElement> & {
-register?: any;
-className?: string;
-firsticonsrc?: FunctionComponent;
+  register?: any;
+  className?: string;
+  firsticonsrc?: FunctionComponent;
 };
 
 export interface ButtonPropsTypes {
-variant?: "google" | "apple";
-title:string
+  variant?: "google" | "apple";
+  title: string;
 }
 
 export type SignUpDataType = {
-userID: string;
-username: string;
-password: string;
-passwordCheck: string;
-email: string;
+  userID: string;
+  username: string;
+  password: string;
+  passwordCheck: string;
+  email: string;
 };
 
 export type LoginDataType = {
-username: string;
-password: string;
+  phoneNumber: string;
 };
 
 export interface NavElementType {
-    text: String, 
-    icon?: FunctionComponent|any,
-    link: String,
-};
+  text: String;
+  icon?: FunctionComponent | any;
+  link: String;
+}
 
 export interface ServiceBoxType {
-    header: string, 
-    body: string, 
-    icon?: FunctionComponent|any,
-    color?: string,
-    link?: string
+  header: string;
+  body: string;
+  icon?: FunctionComponent | any;
+  color?: string;
+  link?: string;
 }
 
 export interface TextBoxType {
-    tittle: string,
-    body: string
+  tittle: string;
+  body: string;
 }
 
 export interface TextSectionType {
-    size: string,
+  size: string;
 }
+export type JoinDataType = LoginDataType;
