8d1ace0327899eab762b931813d696c56caf914a
MohammadMosavat <42.octmst@gmail.com>
2024-03-09 14:25:55 +0330

    remove health care text and add link to navigate login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Link from 'next/link';
-import LoginArt from '/public/login.svg'
 
 export default function Home() {
   return (
     <main>
       <div>
-        Health Care
         <Link href={'/register/login'}>CLick to navigate login page</Link>
       </div>
     </main>
