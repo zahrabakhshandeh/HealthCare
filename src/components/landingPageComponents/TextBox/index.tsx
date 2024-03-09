3f2743b17e280e61d9d29bfa9b2c9f84c3217766
MohammadMosavat <42.octmst@gmail.com>
2024-03-09 22:09:31 +0330

    the description is center now by adding style in thex box component

--- src/components/landingPageComponents/TextBox/index.tsx
+++ src/components/landingPageComponents/TextBox/index.tsx
@@ -1,15 +1,13 @@
 import { TextBoxType } from "@/types";
 
-const TextBox = ({tittle, body}: TextBoxType) => {
-    return(
-        <div className="flex gap-12 items-center md:mx-36 mx-14 mt-16 flex-col min-w-6">
-            <h2 className="text-7xl lg:text-6xl">
-                {tittle}
-            </h2>
-            <p className="xl:text-2xl lg:text-lg text-3xl align-middle">
-                {body}
-            </p>
-        </div>
-    )
-}; 
+const TextBox = ({ tittle, body }: TextBoxType) => {
+  return (
+    <div className="flex gap-12 items-center md:px-36 px-14 flex-col min-w-6">
+      <h2 className="text-7xl lg:text-6xl">{tittle}</h2>
+      <p className="xl:text-2xl lg:text-lg text-center text-3xl align-middle">
+        {body}
+      </p>
+    </div>
+  );
+};
 export default TextBox;
