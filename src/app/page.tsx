2c23a70733ddeb378cbe2d9ed846e601abf83063
ParsaBordbar <parsab71@gmail.com>
2024-03-11 11:27:22 +0330

    Remove component search from project.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import HamburgerNavbar from '@/components/landingPageComponents/HamburgerNavbar'
 import Hero from '@/components/landingPageComponents/Hero';
 import JoinUs from '@/components/landingPageComponents/Joinus';
 import Navbar from '@/components/landingPageComponents/Navbar';
-import Search from '@/components/landingPageComponents/Search';
 import ServiceSection from '@/components/landingPageComponents/SerciveSection';
 import TextSection from '@/components/landingPageComponents/TextSection/indx';
 
@@ -18,9 +17,6 @@ export default function Home() {
         <div className='col-span-full'>
           <Hero />
         </div>
-        {/* <div className='col-span-full '>
-          <Search />
-        </div> */}
         <div className='col-span-full'>
           <ServiceSection />
         </div>
